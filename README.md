2adfd65c710c3a4887ded03501a8047a5366aaa3
Julio Bell <juliobell2014@gmail.com>
2019-05-17 23:13:39 -0400

    All files created and updated

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # liri-node-app
-LIRI is a _Language_ Interpretation and Recognition Interface that can search  Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+LIRI is a _Language_ Interpretation and Recognition Interface that can search  Spotify for songs, Bands in Town for concerts, and OMDB for movies. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+The user has the option of using four commands (listed below) in conjuntion with specific parameters associated with the commands. Liri.js can take in one of the following commands:
+
+1. `node liri.js concert_this <artist/band name here>` 
+    * This searches the Bands in Town Artist Events API for an artist and renders the following information about each event to the terminal:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+2. `node liri.js spotify_this_song <song name here>`
+     * This shows the following information about the song in your terminal/bash window
+
+     * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+   * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+3. `node liri.js movie_this <movie name here>`
+    This outputs the following information to your terminal/bash window:
+    * Title of the movie.
+    * Year the movie came out.
+    * IMDB Rating of the movie.
+    * Rotten Tomatoes Rating of the movie.
+    * Country where the movie was produced.
+    * Language of the movie.
+    * Plot of the movie.
+    * Actors in the movie.
+    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+4. `node liri.js do_what_it_says`
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+How to use Liri.js:
+
+-Open your terminal such as Bash.
+
+-Navigate to the folder that contains the liri.js file.
+
+-Depending on the command you run, the output will vary.
+
+**Example 1**: Run the `concert_this` command
+
+node liri.js concert_this <name of artist or band>
+
+Output: The system will display a list of all events and locations where the artist or band will perform. It can result in multiple records. The system will also log all the results in the log.txt file. See screen-shot below:
+
+![Results](/screenshots/concert_this_results.PNG)
+
+**Example 2**: Run the `spotify_this_song` command
+
+ node liri.js spotify_this_song <name of song>
+
+Output: The system will display a list of information associated with the song. It can result in multiple records. The system will also log all the results in the log.txt file. See screen-shot below:
+
+![Results](/screenshots/spotify_this_song_results.PNG)
+
+**Example 3**: Run the `movie_this` command
+
+ node liri.js movie_this <name of movie>
+
+Output: The system will display information associated with the movie. The system will also log all the results in the log.txt file. See screen-shot below:
+
+![Results](/screenshots/movie_this_results.PNG)
+
+**Example 4**: Run the do_what_it_says command
+
+ node liri.js do_what_it_says
+
+Output: The system will read the text in the random.txt file, and perform the comman listed in the random.txt file.
+
+See screen-shot below:
+
+![Results](/screenshots/do_what_it_says_results.PNG)
+
+TECHNOLOGIES USED
+Javascript
+Nodejs
+Node packages:
+    Node-Spotify-API
+    Request
+    Moment
+    DotEnv
+APIs used:
+    Bands in Town
+    OMDB
+Git
+GitHub
+
--- liri.js
+++ liri.js
@@ -16,22 +16,29 @@ let spotify = new Spotify(keys.spotify);
 
 //Get all the arguments of the input starting in argument in position [3] and concatenating them with a ' ' space.
 let userInput = process.argv.splice(3).join(' ');
+//Get user topic of the input argument in position [2], basically the function used for the search 
+//(concert_this, spotify_this_song, movie_this, or do_what_it_says).
 let userTopic = process.argv[2];      
   
-//This function uses the Bands in Town Artist Events API. 
+//This function uses the Bands in Town Artist Events API to search for concerts or events of artists/bands. 
 //An axios.get sent the search request and the results are console.logged using moment to change the format of the returned date.
 function concert_this (artistName) {
   let queryURL = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
-  console.log("Upcoming Concerts of " + artistName);
+  console.log("===================================" + "\n" + "Upcoming Concerts of " + artistName + "\n" + "===================================" + "\n");
+  logOutputData("===================================" + "\n" + "Upcoming Concerts of " + artistName + "\n" + "===================================" + "\n");
   axios.get(queryURL)
     .then(artistResponse => {
       let events = artistResponse.data;
       events.forEach((item) => {
-      console.log("Name of the venue: ", item.venue.name);
-      console.log("Venue Location: ", item.venue.city);
-      let date = moment(item.datetime);
-      console.log("Date of the event: ", date.format("MM/DD/YYYY"));
+        let date = moment(item.datetime);
+        let eventsData = [
+          "Name of the venue: " + item.venue.name,
+          "Venue Location: " + item.venue.city + ", " + item.venue.country,
+          "Date of the event: " + date.format("MM/DD/YYYY") + "\n"
+        ].join("\n");
       
+      console.log(eventsData);
+      logOutputData(eventsData);
     });
      
     })
@@ -40,10 +47,11 @@ function concert_this (artistName) {
     });
 };
 
-//This function uses the Spotify request API. A node-spotify-api spotify.request sent the search request and the results are console.logged.
+//This function uses the Spotify request API. A node-spotify-api request sent to search for a track or song. Request and the results are console.logged.
 function spotify_this_song(songName) { 
   let queryURL = "https://api.spotify.com/v1/search?q=track:" + songName + "&type=track&limit=1";
-  console.log("Song: " + songName);
+  console.log("======================" + "\n" +"Song: " + songName + "\n" + "======================" + "\n");
+  logOutputData("======================" + "\n" +"Song: " + songName + "\n" + "======================" + "\n");
   //Make a request to Spotify
   spotify.request(queryURL)
   .then(function (songResponse) {
@@ -58,15 +66,16 @@ function spotify_this_song(songName) {
       });
 
       //build trackData block
-      var trackData = [
-        'Artist (s): ' + artistsNames.join(' & '),
-        'Song: ' + track.name,
-        'Album: ' + track.album.name,
-        track.preview_url ? 'Preview URL: ' + track.preview_url : 'Preview URL: ' + 'none provided'
-      ].join('\n');
+      let trackData = [
+        "Artist (s): " + artistsNames.join(' & '),
+        "Song: " + track.name,
+        "Album: " + track.album.name,
+        track.preview_url ? "Preview URL: " + track.preview_url : "Preview URL: " + "none provided" + "\n"
+      ].join("\n");
 
       // console.log(artistsNames.join(' & '));
       console.log(trackData);
+      logOutputData(trackData);
     });
     
   })
@@ -78,20 +87,26 @@ function spotify_this_song(songName) {
 
 };
 
-//This function uses the omdb API. An axios.get sent the search request and the results are console.logged.
+//This function uses the omdb API to search for movies. An axios.get sent the search request and the results are console.logged.
 function movie_this(movieName){
   let queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=92a1ec29";
-  console.log("Watch this movie " + movieName);
+  console.log("===============================" + "\n" +"Watch this movie: " + movieName + "\n" + "===============================" + "\n");
+  logOutputData("===============================" + "\n" +"Watch this movie: " + movieName + "\n" + "===============================" + "\n");
   axios.get(queryURL)
     .then(movieResponse => {
-        console.log("Title: " + movieResponse.data.Title);
-        console.log("Year: " + movieResponse.data.Year);
-        console.log("Rated: " + movieResponse.data.imdbRating);
-        console.log("Country: " + movieResponse.data.Country);
-        console.log("Language: " + movieResponse.data.Language);
-        console.log("Plot: " + movieResponse.data.Plot);
-        console.log("Actors: " + movieResponse.data.Actors);
-           
+        let movieData = [
+          "Title: " + movieResponse.data.Title,
+          "Year: " + movieResponse.data.Year,
+          "Rated: " + movieResponse.data.imdbRating,
+          "Country: " + movieResponse.data.Country,
+          "Plot: " + movieResponse.data.Plot,
+          "Actors: " + movieResponse.data.Actors + "\n"
+        ].join("\n");
+
+        console.log(movieData);
+        logOutputData(movieData);
+        
+                   
     })
     .catch(err => {
       console.log(err);
@@ -115,24 +130,60 @@ function do_What_It_Says() {
 //This function returns the artist name
 function artistInfo(){
   var artistName = "";
-  artistName = userInput;
+  if (userInput){
+    artistName = userInput;
+  }
+  else {
+    artistName = "Default";
+  }
    return artistName;
 };
 //This function returns the track name
   function songInfo(){
     var songName = "";
-    songName = userInput;
+    if (userInput){
+      songName = userInput;
+    }
+    else {
+      songName = "The Sign by Ace of Base"; //default song
+    }
     return songName;
   };
 
   //This function returns the movie name
   function movieInfo(){
     var movieName = "";
-    movieName = userInput;
+    if (userInput) {
+      movieName = userInput;
+    }
+    else {
+      movieName = "Mr. Nobody";//default movie
+    }
+    
     return movieName;
   };
 
-//Handling the user topics
+  //This function appends data to file 'log.txt'
+  function logOutputData(response){
+    fs.appendFile("log.txt", "\n" + response, function(error) {
+      if (error) {
+        return console.log(error);
+      }
+   });
+  };
+ 
+  //This function delete all content in file 'log.txt'
+  function emptyFile(){
+    fs.writeFile("log.txt", " ", function(error) {
+      if (error) {
+        return console.log(error);
+      }
+   });
+  };
+
+  // emptyFile();
+
+//Handling the user topics and executes the corresponding functions
 switch (userTopic) {
   case "concert_this":
         concert_this(artistInfo());
@@ -146,4 +197,7 @@ switch (userTopic) {
   case "do_what_it_says":  
         do_What_It_Says();
         break;
+  default: 
+        console.log("Invalid Option. Please type any of the following options: \nconcert_this \nspotify_this_song \nmovie_this \ndo_what_it_says")
+    
 };
